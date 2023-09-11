52ff6d433d7496ef30319c7c6f8d5d2c01850424
Oluwaseun Sodeeq <ademolaoluwaseun90@gmail.com>
2023-09-11 20:49:33 +0100

    new-commit

--- src/Star/StarRating.js
+++ src/Star/StarRating.js
@@ -1,37 +1,89 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
 const containerStyle = { display: "flex", alignItem: "center", gap: "16px" };
-const textStyle = { margin: "0", lineHeight: "1" };
 const starsContainerStyle = { display: "flex", gap: "4px" };
-const starStyle = { width: "40px", height: "40px", display: "block" };
 
-const StarRating = ({ maxRaing = 5 }) => {
-  const [rating, setRating] = useState(0);
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  defaultRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  className: PropTypes.string,
+  onsetMovieRating: PropTypes.func,
+};
+
+const StarRating = ({
+  maxRating = 5,
+  color = "#fcc419",
+  size = "48",
+  className = "",
+  message = [],
+  defaultRating = 0,
+  onsetMovieRating,
+}) => {
+  const textStyle = {
+    margin: "0",
+    lineHeight: "1",
+    fontSize: `${size / 1.5}px`,
+    color,
+  };
+
+  const [rating, setRating] = useState(defaultRating);
+  const [temprating, setTemprating] = useState(0);
+
   function handleRating(rating) {
     setRating(rating);
+    onsetMovieRating(rating);
   }
+  // function handleTempRating() {
+  //   setTemprating(temprating);
+  //   console.log("hi");
+  // }
+  console.log(message);
   return (
     <>
-      <div style={containerStyle}>
+      <div style={containerStyle} className={className}>
         <div style={starsContainerStyle}>
-          {Array.from({ length: maxRaing }, (_, i) => (
+          {Array.from({ length: maxRating }, (_, i) => (
             <Star
               key={i + 1}
               onRate={() => handleRating(i + 1)}
-              full={rating >= i + 1}
+              full={temprating ? temprating >= i + 1 : rating >= i + 1}
+              onHoverIn={() => setTemprating(i + 1)}
+              onHoverOut={() => setTemprating(0)}
+              color={color}
+              size={size}
             />
           ))}
         </div>
-        <p style={textStyle}>{rating || ""}</p>
+        <p style={textStyle}>
+          {message.length === maxRating
+            ? message[temprating ? temprating - 1 : rating - 1]
+            : temprating || rating || ""}
+        </p>
       </div>
     </>
   );
 };
 
-function Star({ onRate, full }) {
-  const color = "#fcc419";
+function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
+  const starStyle = {
+    width: `${size}px`,
+    height: `${size}px`,
+    display: "block",
+    cursor: "pointer",
+    color,
+  };
+  // const color = "#fcc419";
   return (
-    <span style={starStyle} role="button" onClick={onRate}>
+    <span
+      style={starStyle}
+      role="button"
+      onClick={onRate}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+    >
       {full ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -86,3 +138,59 @@ export default StarRating;
             />
       </svg>
  */
+/*
+import "./styles.css";
+import {useState} from "react"
+export default function App() {
+  return (
+    <div>
+      <TextExpander>
+        Space travel is the ultimate adventure! Imagine soaring past the stars
+        and exploring new worlds. It's the stuff of dreams and science fiction,
+        but believe it or not, space travel is a real thing. Humans and robots
+        are constantly venturing out into the cosmos to uncover its secrets and
+        push the boundaries of what's possible.
+      </TextExpander>
+
+      <TextExpander
+        collapsedNumWords={20}
+        expandButtonText="Show text"
+        collapseButtonText="Collapse text"
+        buttonColor="#ff6622"
+      >
+        Space travel requires some seriously amazing technology and
+        collaboration between countries, private companies, and international
+        space organizations. And while it's not always easy (or cheap), the
+        results are out of this world. Think about the first time humans stepped
+        foot on the moon or when rovers were sent to roam around on Mars.
+      </TextExpander>
+
+      <TextExpander expanded={true} className="box">
+        Space missions have given us incredible insights into our universe and
+        have inspired future generations to keep reaching for the stars. Space
+        travel is a pretty cool thing to think about. Who knows what we'll
+        discover next!
+      </TextExpander>
+    </div>
+  );
+}
+
+function TextExpander(
+  {collapsedNumWords = 10,
+    expandButtonText ="Show more",
+    collapseButtonText ="show less",
+    buttonColor = "#1ff09cd",
+    expanded = false,
+    className,children}) {
+  const [isExpanded,setExpanded] = useState(expanded)
+  const displayText = isExpanded ? children : children.split(" ").slice(0, collapsedNumWords).join(" ") + "..."
+  const buttonStyles = {
+background: "none",border: "none",font: "none",cursor: "pointer",color:buttonColor
+  }
+  return( <div className={className}>
+    <span>{displayText}</span>
+    <button onClick={()=> setExpanded((exp) => !exp)} style={buttonStyles}>{isExpanded ? collapseButtonText :  expandButtonText}</button>
+    </div>);
+}
+
+*/
--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./Star/StarRating";
-
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating
+        color="green"
+        maxRating={10}
+        onsetMovieRating={setMovieRating}
+      />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <>
-      <StarRating maxRaing={5} />
-      <StarRating maxRaing={7} />
+      <StarRating
+        maxRaing={5}
+        message={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      />
+      <StarRating color="blue" size="24" defaultRating={3} />
+      {/* //this uses the default max of Rate/Stars */}
+      <StarRating maxRaing={7} color="red" size="60" className="myStarStyles" />
+      <Test />
     </>
   </React.StrictMode>
 );
